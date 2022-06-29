21cfc3f640c35f1fab88bb79c2e2ed28262c13ba
Therealisticblog <103075116+Therealisticblog@users.noreply.github.com>
2022-06-29 19:13:41 +0100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,12 @@
+function lightmode(){
+    document.body.style.backgroundColor="white";
+    document.body.style.color="black";
+    document.getElementById("button").style.backgroundColor='#202124';
+    document.getElementById("button").style.color="white";
+    document.getElementById("button").innerHTML="Dark Mode";
+    document.getElementById("nav-links").style.color="black";
+    document.getElementById("nav-links1").style.color="black";
+    document.getElementById("nav-links2").style.color="black";
+    document.getElementById("nav-links3").style.color="black";
+    document.getElementById("navbar").style.backgroundColor="white";
+}
